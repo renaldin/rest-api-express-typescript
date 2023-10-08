4827bc29ab4ac600a2f1d820e16a6a7790e8699c
Renaldi Noviandi <renaldinoviandi0@gmail.com>
2023-10-08 19:42:22 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({
+    response: "Express Typescript",
+  });
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(
+    `${process.env.APP_NAME}: running on port ${process.env.APP_PORT}`
+  );
+});
